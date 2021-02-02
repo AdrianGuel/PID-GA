374c79b1e93f0524e7c7e696431631c3884c25f7
A.-J. Guel-Cortez <52336176+AdrianGuel@users.noreply.github.com>
2021-02-02 20:04:42 +0000

    Update PIDGA.m

--- PIDGA.m
+++ PIDGA.m
@@ -1,3 +1,7 @@
+%Autor Adrian Josue Guel Cortez 2020
+%Please cite one of my works if you are using this algorithm.
+%https://scholar.google.com.mx/citations?user=gZcBLuoAAAAJ&hl=es
+
 clear all
 close all
 timerVal=tic;
